58d037f5199f6c8a87fd5e443c020908bb00feb4
Martin Morgan <mtmorgan@fhcrc.org>
2017-04-28 23:25:02 +0000

    copy RITANdata from trunk
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/branches/RELEASE_3_5/experiment/pkgs/RITANdata@4162 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/geneset_list.R
@@ -0,0 +1,9 @@
+#' @title geneset_list.RData
+#' @description R list object containing annotation resources and the gene sets contained in each of them. 
+#' @format A \code{list} with one element for each annotation resource. Each element contains a varying number of gene symbols.
+#' @source Multiple sources used. See our table in the network vignette.
+#' @return A list of resources. Each resource contains a list of genesets.
+#' @author Michael T Zimmermann
+#' @examples
+#' print(names(geneset_list))
+"geneset_list"
